bbc07854cca2751478fd0c99f54d3cfa2d4feb1f
aiamber <lzlrjok@vip.qq.com>
2017-12-31 22:19:20 +0800

    20171231 save to db-v1.0.1

--- test2/shdbData.py
+++ test2/shdbData.py
@@ -44,14 +44,14 @@ def reqData(url, dataNum):
 		dsls.append(dsl)
 	# print(dsls)
 
-# dt = reqData(url, dataNum)
-	print(dsls)
+	# dt = reqData(url, dataNum)
+	# print(dsls)
 
 	name = 'sz000672'
 	volume = int(dsls[8])
 	turnVolume = float(dsls[9])
 	timeStamp = dsls[30] + ' ' + dsls[31]
-	print(volume, turnVolume, timeStamp)
+	# print(volume, turnVolume, timeStamp)
 
 	cur.execute("INSERT INTO %s (time_stamp ,stock_name, price, open_tod, close_lst, high, low, sel_5, sel_5_num, sel_4, sel_4_num, sel_3, sel_3_num, sel_2, sel_2_num, sel_1, sel_1_num, buy_1, buy_1_num, buy_2, buy_2_num, buy_3, buy_3_num, buy_4, buy_4_num, buy_5, buy_5_num, volume, turn_volume) VALUES ('%s', '%s', %f, %f, %f, %f, %f, %f, %d, %f, %d, %f, %d, %f, %d, %f, %d, %f, %d, %f, %d, %f, %d, %f, %d, %f, %d, %d, %f)" %(tableName, timeStamp, name, float(dsls[3]), float(dsls[1]), float(dsls[2]), float(dsls[4]), float(dsls[5]), float(dsls[29]), int(dsls[28]), float(dsls[27]), int(dsls[26]), float(dsls[25]), int(dsls[24]), float(dsls[23]), int(dsls[22]), float(dsls[21]), int(dsls[20]), float(dsls[11]), int(dsls[10]), float(dsls[13]), int(dsls[12]), float(dsls[15]), int(dsls[14]), float(dsls[17]), int(dsls[16]), float(dsls[19]), int(dsls[20]), volume, turnVolume ) )
 
@@ -59,7 +59,10 @@ def reqData(url, dataNum):
 	# conn.close()
 	# print("Success!")
 
-while ins < 4788:
+while ins < 20:
 	reqData(url, dataNum)
 	dsls=[]
 	time.sleep(1.7)
+	ins += 1
+
+exit()
